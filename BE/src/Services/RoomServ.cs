f810ca62ea831f7ae95aeabc308148892689d850
OtakuIsMe <duynmse173649@fpt.edu.vn>
2024-10-17 22:15:39 +0700

    .

--- BE/src/Services/RoomServ.cs
+++ BE/src/Services/RoomServ.cs
@@ -253,5 +253,27 @@ namespace BE.src.Services
                 return ErrorResp.BadRequest(ex.Message);
             }
         }
+        public async Task<IActionResult> TrendingRoom(){
+            try
+            {
+                return SuccessResp.Ok("");
+            }
+            catch (System.Exception ex)
+            {
+                return ErrorResp.BadRequest(ex.Message);
+            }
+        }
+        public async Task <IActionResult> GetRoomListWithBookingTimes(Guid areaId, TypeRoomEnum typeRoom, DateTime startDate, DateTime endDate)
+        {
+            try
+            {
+                List<Room> rooms = await _roomRepo.GetRoomListWithBookingTimes(areaId, typeRoom, startDate, endDate);
+                return SuccessResp.Ok(rooms);
+            }
+            catch (System.Exception ex)
+            {
+                return ErrorResp.BadRequest(ex.Message);
+            }
+        }
     }
 }
