a4c5ecd35ef2231c061d62e3bb534277ab080b0d
Pi2608 <ibaxq@gmail.com>
2024-10-25 08:54:10 +0700

    syntax error fix

--- BE/src/Services/TransactionServ.cs
+++ BE/src/Services/TransactionServ.cs
@@ -5,7 +5,7 @@ using BE.src.Repositories;
 using BE.src.Shared.Constant;
 using BE.src.Shared.Type;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Infrasstructure;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using PayPal.Api;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
@@ -25,7 +25,6 @@ namespace BE.src.Services
         Task<IActionResult> PaymentByPaypal(PaymentPayPalDto data);
         Task<IActionResult> PaymentPaypalSuccess(Guid bookingId);
         Task<IActionResult> PaymentByCod(Guid bookingId);
-        Task<IActionResult> StatisticMonthInYear(int year);
     }
 
     public class TrasactionServ : ITransactionServ
@@ -50,21 +49,24 @@ namespace BE.src.Services
 
         private Payment CreatePayment(float total, string returnUrl, string cancelUrl)
         {
-            var apiContext = GetAPIContext();
-
-            decimal newTotal = Math.Round((decimal)total / 24850, 2);
-            Console.WriteLine(newTotal);
-
-            var payment = new Payment
+            try
             {
-                intent = "sale",
-                payer = new Payer { payment_method = "paypal" },
-                redirect_urls = new RedirectUrls
+                var apiContext = GetAPIContext();
+
+                // Math.Round((decimal)total / 24850, 2)
+                decimal newTotal = 10M;
+                Console.WriteLine(newTotal);
+
+                var payment = new Payment
                 {
-                    cancel_url = cancelUrl,
-                    return_url = returnUrl
-                },
-                transactions = new List<PaypalTransaction>
+                    intent = "sale",
+                    payer = new Payer { payment_method = "paypal" },
+                    redirect_urls = new RedirectUrls
+                    {
+                        cancel_url = cancelUrl,
+                        return_url = returnUrl
+                    },
+                    transactions = new List<PaypalTransaction>
                 {
                     new PaypalTransaction
                     {
@@ -77,10 +79,17 @@ namespace BE.src.Services
                         }
                     }
                 }
-            };
+                };
 
-            return payment.Create(apiContext);
+                return payment.Create(apiContext);
+            }
+            catch (PayPal.PaymentsException ex)
+            {
+                Console.WriteLine(ex.Response);
+                return null;
+            }
         }
+
 
         public async Task<IActionResult> PaymentByPaypal(PaymentPayPalDto data)
         {
@@ -92,8 +101,8 @@ namespace BE.src.Services
                 {
                     return ErrorResp.NotFound("Cant find booking");
                 }
-                string return_url = $"http://localhost:5101/transaction/Payment-PayPal-Success?bookingId={data.BookingId}";
-                string cancel_url = "http://localhost:5173/";
+                string return_url = "https://www.youtube.com/";
+                string cancel_url = "https://www.youtube.com/";
                 var payment = CreatePayment(booking.Total, return_url, cancel_url);
                 var approvalUrl = payment.links.FirstOrDefault(lnk => lnk.rel.Equals("approval_url", StringComparison.OrdinalIgnoreCase))?.href;
                 return SuccessResp.Ok(approvalUrl);
@@ -139,7 +148,7 @@ namespace BE.src.Services
                     PointBonus = 0,
                     BookingId = bookingId,
                     PaymentType = PaymentTypeEnum.Paypal,
-                    Status = true
+                    Satutus = true
                 };
                 bool isCreatedPayment = await _transactionRepo.CreatePaymentRefund(payment);
                 if (!isCreatedPayment)
@@ -188,7 +197,7 @@ namespace BE.src.Services
                     PointBonus = 0,
                     BookingId = bookingId,
                     PaymentType = PaymentTypeEnum.COD,
-                    Status = false
+                    Satutus = false
                 };
                 bool isCreatedPayment = await _transactionRepo.CreatePaymentRefund(payment);
                 if (!isCreatedPayment)
@@ -208,80 +217,6 @@ namespace BE.src.Services
                     return ErrorResp.BadRequest("Cant create transaction");
                 }
                 return SuccessResp.Ok("Payment cod done");
-            }
-            catch (System.Exception ex)
-            {
-                return ErrorResp.BadRequest(ex.Message);
-            }
-        }
-
-        public async Task<IActionResult> StatisticMonthInYear(int year)
-        {
-            try
-            {
-                var transactions = await _transactionRepo.TransactionInYear(year);
-                List<StatisticMonth> returnValue = new(){
-                    new StatisticMonth(){
-                        Month = 1,
-                        Amount = 0
-                    },
-                    new StatisticMonth(){
-                        Month = 2,
-                        Amount = 0
-                    },
-                    new StatisticMonth(){
-                        Month = 3,
-                        Amount = 0
-                    },
-                    new StatisticMonth(){
-                        Month = 4,
-                        Amount = 0
-                    },
-                    new StatisticMonth(){
-                        Month = 5,
-                        Amount = 0
-                    },
-                    new StatisticMonth(){
-                        Month = 6,
-                        Amount = 0
-                    },
-                    new StatisticMonth(){
-                        Month = 7,
-                        Amount = 0
-                    },
-                    new StatisticMonth(){
-                        Month = 8,
-                        Amount = 0
-                    },
-                    new StatisticMonth(){
-                        Month = 9,
-                        Amount = 0
-                    },
-                    new StatisticMonth(){
-                        Month = 10,
-                        Amount = 0
-                    },
-                    new StatisticMonth(){
-                        Month = 11,
-                        Amount = 0
-                    },
-                    new StatisticMonth(){
-                        Month = 12,
-                        Amount = 0
-                    },
-
-                };
-                foreach (var transaction in transactions)
-                {
-                    foreach (var statisticMonth in returnValue)
-                    {
-                        if (transaction.CreateAt.HasValue && statisticMonth.Month == transaction.CreateAt.Value.Month)
-                        {
-                            statisticMonth.Amount += transaction.Total;
-                        }
-                    }
-                }
-                return SuccessResp.Ok(returnValue);
             }
             catch (System.Exception ex)
             {
